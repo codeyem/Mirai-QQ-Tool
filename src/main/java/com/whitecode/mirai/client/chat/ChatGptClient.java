61a9f7418f86aae0ff378e356aaa02fdee7c852d
yem <yem@shjysoft.com>
2023-04-25 18:04:47 +0800

    改下密钥

--- src/main/java/com/whitecode/mirai/client/chat/ChatGptClient.java
+++ src/main/java/com/whitecode/mirai/client/chat/ChatGptClient.java
@@ -5,8 +5,10 @@ import cn.hutool.http.HttpResponse;
 import com.google.gson.Gson;
 import com.whitecode.mirai.client.model.ChatGptModel;
 import com.whitecode.mirai.client.model.response.ChatGptResponseModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,13 +19,24 @@ import java.util.Map;
  * @Description:
  */
 @Component
+@Slf4j
 public class ChatGptClient {
     private static final String url = "https://api.openai.com/v1/chat/completions";
+
+    private static String secret = null;
+
+    static {
+        try {
+            secret = new String(Base64.getDecoder().decode("c2stRkFUaGo3NEhIa0pMS0RVanV5NmdUM0JsYmtGSldrRzdwY3U1S1hPSmdqUkJOMElV"),"utf-8");
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+        }
+    }
 
     public String messgae(String message){
         Map<String, String> headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/json");
-        headers.put("Authorization", "Bearer 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");
+        headers.put("Authorization", secret);
         String json = new Gson().toJson(ChatGptModel.getDefault(message));
         HttpRequest request = HttpRequest.post(url).addHeaders(headers).body(json);
         request.setHttpProxy("127.0.0.1", 7890);
--- src/main/java/com/whitecode/mirai/properties/ChatGptProperties.java
+++ src/main/java/com/whitecode/mirai/properties/ChatGptProperties.java
@@ -1,10 +1,17 @@
 package com.whitecode.mirai.properties;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 /**
  * @ClassName:ChatGptProperties.java
  * @Author:Yem
  * @CreateTime:2023-04-25
  * @Description:
  */
+@Data
+@Component
+@ConfigurationProperties("mirai.chat-gpt")
 public class ChatGptProperties {
 }
